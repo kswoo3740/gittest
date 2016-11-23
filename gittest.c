bb094d7324885ed8d72bb65eda2491033a9fce4b
kswoo3740 <kswoo3740@naver.com>
2016-11-23 13:48:15 +0900

    change 3lines

--- gittest.c
+++ gittest.c
@@ -6,7 +6,7 @@ int main () {
   for (i = 0; i < 100; i++) {
     printf ("Hansol bothered %d times\n", i);
   }
-  printf ("SangU killed himself\n");
-  printf ("Make conflict\n");
-  printf ("What the Hansol\n");
+  printf ("SangU killed Hansol\n");
+  printf ("Remove conflict\n");
+  printf ("What the hell Hansol\n");
 }
