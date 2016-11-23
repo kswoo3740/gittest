7966e5141618900c24f87f24bb8742b5685cdfb3
kswoo3740 <kswoo3740@naver.com>
2016-11-23 13:26:00 +0900

    first commit

---
+++ gittest.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+  int i;
+  for (i = 0; i < 100; i++) {
+    printf ("Hansol bothered SangU %d times\n", i);
+  }
+  printf ("SangU killed Hansol\n");
+}
